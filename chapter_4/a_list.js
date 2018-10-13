6803b532104d5bef689064d0bda3dbc3018b73eb
Cédric David <cedric.david@btopenworld.com>
2018-10-14 00:54:54 +1000

    add a_list.js

---
+++ chapter_4/a_list.js
@@ -0,0 +1,43 @@
+const arrayToList = (arr) => {
+  let rest = null;
+  let list;
+  for (let i = arr.length; i >=0; i--) {
+    list = {value: arr[i], rest: list};
+  }
+  return list;
+}
+const listToArray = (list) => {
+  arr = [];
+  while (list.rest != null) {
+    arr.push(list.value);
+    list = list.rest;
+  }
+  return arr;
+}
+
+const prepend = (value, list) => { 
+  return {value, rest: list};
+}
+      
+const nth = (list, num) => {
+  let output; 
+  for(i = 0; i <= num; i++) {
+    if(list.rest == null && i != 0) {
+      output = undefined ;
+    }else{
+      output = list.value;
+      list = list.rest;
+    }
+  }
+  return output;
+ }
+    
+    
+console.log(arrayToList([10, 20]));
+// → {value: 10, rest: {value: 20, rest: null}}
+console.log(listToArray(arrayToList([10, 20])));
+// → [10, 20]
+console.log(prepend(10, prepend(20, null)));
+// → {value: 10, rest: {value: 20, rest: null}}
+console.log(nth(arrayToList([10, 20, 30, 40, 50, 60]), 5));
+// → 20
